e1e3cc158b4b87bee9555e34e23a45f502009ebc
Tuic242 <jeremy242mou@gmail.com>
2019-02-25 21:06:53 +0100

    Create main.js

---
+++ .gitignore/main.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+const express = require('express');
+const app = express();
+
+//Debut Parametres Heroku 
+app.set('port',(process.env.PORT || 5000))
+
+
+bot.on('ready', () => {
+    bot.user.setPresence({ game: { name: 'Bot By Tuic242 [Dit.exe]', type: 0}});
+    console.log("Bot Ready !");
+})
+
+
+bot.on('message', function (message) {
+    if (message.content === 'Dit.exe') {
+        message.channel.send('**Mon préfix est ".exe" mais je suis pour le moment inutilisable**')
+    }
+})
+
+bot.on('message', function (message) {
+    if (message.content === 'Help.exe') {
+        message.channel.send('** Voici toute mais commande : ** \n***Rappelle : je suis toujours en développement *** \n**[InfoJ.exe] Permet de connaitre le statut des métiers **')
+    }
+})
+
+bot.on('message', function (message) {
+    if (message.content === 'Test1.exe') {
+        message.channel.send('`Test1.exe Mise a jour bien intégrer`')
+    }
+})
+
+bot.on('message', function (message) {
+    if (message.content === 'InfoJ.exe') {
+        message.channel.send('**Voici les statut des recrutement de tout les metier : ** \n:cop: **Gendarme : `Close #recrutement-gn`** \n:fire_engine: **Sapeurs Pompiers : `Open #recrutement_sp`** \n:customs: **Douane : `Close #recrutement-dn` ** \n:construction_worker: **Dir : `Open #recrutement-dir`**')
+    }
+})
+
+
+bot.login('NTQ4MTI1OTM1NjYxMjE5ODQx.D1CJqQ.64bpFaJYLOAeYviXJG6mgl9ahVg')
+
+
+
