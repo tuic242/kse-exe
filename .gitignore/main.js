368f1b9ffb970a400c4ddf980cf7aa95ebcaf662
Tuic242 <jeremy242mou@gmail.com>
2019-02-26 18:26:41 +0100

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -105,7 +105,7 @@ bot.on('message', function (message) {
 
 bot.on('message', function (message) {
     if (message.content === 'P1.exe') {
-        message.guilds.send("**Ceci est message teste….. Exécuter par Tuic242**")
+        message.messageNotifications('Test.')
     }
 })
 
