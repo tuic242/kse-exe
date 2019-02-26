6714c7a7fa6f71bee6af3e5bc5289e04c154fc0f
Tuic242 <jeremy242mou@gmail.com>
2019-02-26 09:31:51 +0100

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -3,6 +3,15 @@ const bot = new Discord.Client()
 const express = require('express');
 const app = express();
 
+const role = message.guild.roles.find(role => role.name.toLowerCase() === roleArgs.join(" ").toLowerCase())
+if (!role) return message.reply('There is not such a role!');
+
+for (let i = 0; i < message.guild.members.size; i++) {
+    if (message.guild.members[i].roles.has(role.id)) {
+        message.guild.members[i].user.send(messageArgs.join(" "))
+    }
+}
+
 
 //Debut Parametres Heroku 
 app.set('port',(process.env.PORT || 5000))
