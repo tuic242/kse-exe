1c583f6461400e8897af63bfc0c32f7120ad5a86
Tuic242 <jeremy242mou@gmail.com>
2020-12-06 00:19:44 +0100

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -1,6 +1,5 @@
 const { Client, RichEmbed } = require('discord.js');
 const bot = new Client();
-
 const express = require('express');
 const app = express();
 
@@ -59,8 +58,8 @@ bot.on('message', function (message) {
 
 
 bot.on('message', function (message) {
-    if (message.content === 'InfoJ.exe') {
-        const embed = new bot.RichEmbed()
+    if (message.content === 'Info') {
+        const embed = new RichEmbed()
         .setTitle("__**Status sur les recrutements metiers en cours :**__")
         .setColor('#2e1400')
         .setDescription("👮 **Gendarmerie Nationale =** Recrutement Ouvert \n 🚒 **Sapeurs Pompiers =** Recrutement Ouvert \n 👷**D.I.R =** Recrutement fermé \n \:medical_symbol: **Securite Civile =** Recrtuement Ouvert")
