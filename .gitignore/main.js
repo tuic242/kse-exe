66fce3c97bf3d6996a750772b0b9b80fa2ee7f96
Tuic242 <jeremy242mou@gmail.com>
2020-12-06 00:13:00 +0100

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -59,7 +59,7 @@ bot.on('message', function (message) {
 
 bot.on('message', function (message) {
     if (message.content === 'InfoJ.exe') {
-        const embed = new Client.RichEmbed()
+        const embed = new Client.RichEmbed();
         .setTitle("__**Status sur les recrutements metiers en cours :**__")
         .setColor('#2e1400')
         .setDescription("👮 **Gendarmerie Nationale =** Recrutement Ouvert \n 🚒 **Sapeurs Pompiers =** Recrutement Ouvert \n 👷**D.I.R =** Recrutement fermé \n \:medical_symbol: **Securite Civile =** Recrtuement Ouvert")
