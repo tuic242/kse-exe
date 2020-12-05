0d13d65d4c96cdf7c422633c2ecb0a4f974fc503
Tuic242 <jeremy242mou@gmail.com>
2020-12-05 13:49:13 +0100

    Add files via upload

---
+++ .gitignore/first-message.js
@@ -0,0 +1,26 @@
+const addReactions = (message, reactions) => {
+    message.react(reactions[0])
+    reactions.shift()
+    if (reactions.length > 0) {
+        setTimeout(() => addReactions(message, reactions), 750)
+    }
+}
+
+module.exports = async (client, id, test, reactions = []) => {
+    const channel = await client.channels.fetch(id)
+
+    channel.message.fetch().then((message) => {
+        if (message.size === 0) {
+            // rienr eirneirnre
+            channel.send(text).then((message) => {
+                addReactions(message, reactions)
+            })
+        } else {
+            // rienriejrnre
+            for (const message of messages) {
+                message[1].edit(text)
+                addReactions(message[1], reactions)
+          }
+      }
+  })
+}
