b22177366788a89b4f1d40486f7972e72261bbc7
Tuic242 <jeremy242mou@gmail.com>
2020-12-05 13:57:33 +0100

    Update role-claim.js

--- .gitignore/role-claim.js
+++ .gitignore/role-claim.js
@@ -3,24 +3,6 @@ const firstMessage = require('./first-message')
 module.exports = (client) => {
     const channelId = '696684380374171740'
 
-    const getEmoji = (emojiName) => 
-      client.emojis.cache.find((emoji) => emoji.name === emojiname)
+    firstMessage(client, channelId, 'hello', [])
 
-    const emojis = {
-        NotificationMiniJeux: 'video_game',
-        NotificationStream: 'red_circle',
-    }
-
-    const reaction = []
-
-    let emojitext = 'Clique sur une des réactions pour avoir le rôle que tu veux. \n\n'
-    for (const key in emojis) {
-        const emoji = getEmoji(key)
-        reaction.push(emoji)
-
-        const role = emoji[key]
-        emojiText += `${emoji} = ${role}\n`
-    }
-
-    firstMessage(client, channelId, emojitext, [])
 }
