be868b3878207836e0967861713c0906da85e6ce
Tuic242 <jeremy242mou@gmail.com>
2020-12-24 02:08:53 +0100

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -13,7 +13,7 @@ app.set('port',(process.env.PORT || 5000))
 bot.on('ready', () => {
 	console.log('Bot Launched..')
 	bot.user.setStatus('Do not disturb')
-        bot.user.setActivity('Joyeux Noel', ({type: "STREAMING"}))
+        bot.user.setActivity('Joyeux Noel', ({type: "WATCHING"}))
 });
 
 bot.on('guildMemberAdd', member => {
