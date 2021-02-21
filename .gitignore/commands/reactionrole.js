c9e2cbc66adf474ce68ae1f9651d586f51409aaa
Tuic242 <jeremy242mou@gmail.com>
2021-02-21 12:34:52 +0100

    Create reactionrole.js

---
+++ .gitignore/commands/reactionrole.js
@@ -0,0 +1,70 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION" ]});
+
+const prefix = '_';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+ 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+ 
+    client.commands.set(command.name, command);
+}
+
+client.on('ready', () => {
+    console.log(`En ligne`)
+    client.user.setActivity('le groupe KSE', ({type: "WATCHING"}))
+})
+
+client.on('guildMemberAdd', (member) => {
+    let channelID = '788188331267457086';
+    if(member.guild.id != '788188330519953410') return;
+    let embed = new Discord.MessageEmbed()
+    .setTitle('Bienvenue 👋 !')
+    .setDescription(`${member.user.tag} Bonjours a toi ! Hesite pas aller lire le reglement.`)
+    .setColor('#2e1400')
+    .setTimestamp()
+    client.channels.cache.get(channelID).send(embed)
+})
+client.on('guildMemberRemove', (member) => {
+    let channelID = '788188331267457086';
+    if(member.guild.id != '788188330519953410') return;
+    let embed = new Discord.MessageEmbed()
+    .setTitle('A Bientot !')
+    .setDescription(`${member.user.tag} Quitte le serveur.`)
+    .setColor('#2e1400')
+    .setTimestamp()
+    client.channels.cache.get(channelID).send(embed)
+})
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('ping #brain')
+    }
+    if(command === 'help'){
+        message.channel.send('***En cours de redaction***')
+    }
+})
+
+client.on('message', message => {
+ 
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    if (command === 'reactionrole') {
+        client.commands.get('reactionrole').execute(message, args, Discord, client);
+    } 
+  
+});
+
+client.login('ODExNzEzMjg0Nzk2NTc5OTMy.YC2M5w.-WqNBSg72Lam3VEFiwnbCRaSlCU');
