dca7c2fb1a43b4463c3e569cd54ebffaee97a05b
Tuic242 <jeremy242mou@gmail.com>
2021-03-31 14:23:42 +0200

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -4,7 +4,7 @@ const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION" 
 
 client.on('ready', () => {
     console.log(`En ligne`)
-    client.user.setActivity('V0.11', ({type: "WATCHING"}))
+    client.user.setActivity('V0.12', ({type: "WATCHING"}))
 })
 
 client.on('guildMemberAdd', (member) => {
@@ -26,6 +26,20 @@ client.on('guildMemberRemove', (member) => {
     .setColor('#2e1400')
     .setTimestamp()
     client.channels.cache.get(channelID).send(embed)
+})
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('ping #brain')
+    }
+    if(command === 'help'){
+        message.channel.send('***En cours de redaction***')
+    }
 })
 
 client.login(process.env.TOKEN)
