44e6860d6ac2cfceee04153e48cc6548fecb6de3
Tuic242 <jeremy242mou@gmail.com>
2021-03-31 14:39:20 +0200

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -2,16 +2,27 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION" ]});
 
-var prefix = '_'; 
-      
+const prefix = '_';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+ 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+ 
+    client.commands.set(command.name, command);
+}
+
 client.on('ready', () => {
     console.log(`En ligne`)
-    client.user.setActivity('V0.12', ({type: "WATCHING"}))
+    client.user.setActivity('En cours de test...', ({type: "WATCHING"}))
 })
 
 client.on('guildMemberAdd', (member) => {
-    let channelID = '613703562060496922';
-    if(member.guild.id != '788188330519953410') return;
+    let channelID = '613707298350563328';
+    if(member.guild.id != '354263712430948354') return;
     let embed = new Discord.MessageEmbed()
     .setTitle('Bienvenue 👋 !')
     .setDescription(`${member.user.tag} Bonjours a toi ! Hesite pas aller lire le reglement.`)
@@ -20,8 +31,8 @@ client.on('guildMemberAdd', (member) => {
     client.channels.cache.get(channelID).send(embed)
 })
 client.on('guildMemberRemove', (member) => {
-    let channelID = '613703562060496922';
-    if(member.guild.id != '788188330519953410') return;
+    let channelID = '613707298350563328';
+    if(member.guild.id != '354263712430948354') return;
     let embed = new Discord.MessageEmbed()
     .setTitle('A Bientot !')
     .setDescription(`${member.user.tag} Quitte le serveur.`)
@@ -43,5 +54,17 @@ client.on('message', message => {
         message.channel.send('***En cours de redaction***')
     }
 })
+
+client.on('message', message => {
+ 
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    if (command === 'reactionrole') {
+        client.commands.get('reactionrole').execute(message, args, Discord, client);
+    } 
+  
+});
 
 client.login(process.env.TOKEN)
