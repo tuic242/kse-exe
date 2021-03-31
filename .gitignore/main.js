124b23a50ff71d22b2ecc352a308887a8afee113
Tuic242 <jeremy242mou@gmail.com>
2021-03-31 14:26:25 +0200

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION" ]});
 
-const prefix = '_'; 
+var prefix = '_'; 
       
 client.on('ready', () => {
     console.log(`En ligne`)
