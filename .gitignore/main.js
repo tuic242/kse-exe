58500b4b307e1c678dd75295d50b46f9259553a9
Tuic242 <jeremy242mou@gmail.com>
2021-04-13 12:45:22 +0200

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 
+const { Client, RichEmbed } = require('discord.js');
+
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION" ]});
 
 const prefix = '_';
