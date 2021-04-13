4a2fa0da3471f68f1c5ca537c8a026afd4044837
Tuic242 <jeremy242mou@gmail.com>
2021-04-13 12:38:37 +0200

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -17,7 +17,7 @@ for (const file of commandFiles) {
 
 client.on('ready', () => {
     console.log(`En ligne`)
-    client.user.setActivity('Restez Chez Vous', ({type: "WATCHING"}))
+    client.user.setActivity('CHANGEMENT DE GROUPE ROBLOX !', ({type: "WATCHING"}))
 })
 
 client.on('guildMemberAdd', (member) => {
